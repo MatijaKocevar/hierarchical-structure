bbe80c73625cfedcbbd1f100faabeec2dfea0384
Matija Kočevar <matija.kocev@gmail.com>
2025-06-09 22:12:29 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="h-full flex items-center justify-center">
+            <div>Hierarchical structure</div>
+        </div>
+    );
+}
+
+export default App;
