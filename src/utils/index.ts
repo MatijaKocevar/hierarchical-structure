457759f8f7fb193542297653140fc6405ed7ea34
Matija Kočevar <matija.kocev@gmail.com>
2025-06-15 21:36:48 +0200

    Remove unused util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 import type { Item } from "../types";
 export * from "./dataGenerator";
 export * from "./flattenData";
-export * from "./radialPoint";
 
 export const countLeafNodes = (node: Item | null): number => {
     if (!node) return 0;
--- src/utils/radialPoint.ts
+++
@@ -1,3 +0,0 @@
-export function radialPoint(x: number, y: number): [number, number] {
-    return [y * Math.cos(x - Math.PI / 2), y * Math.sin(x - Math.PI / 2)];
-}
