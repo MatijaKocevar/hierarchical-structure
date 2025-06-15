fcedb92cd69a53ed87e09b9aef04d094080c82f0
Matija Kočevar <matija.kocev@gmail.com>
2025-06-15 19:39:28 +0200

    Update base config for development

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: "/hierarchical-structure/",
+export default defineConfig(({ mode }) => ({
+    base: mode === "production" ? "/hierarchical-structure/" : "/",
     plugins: [react(), tailwindcss()],
-});
+}));
